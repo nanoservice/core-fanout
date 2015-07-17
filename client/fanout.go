6fec302411b6df8cbc7450d46bf96df827f2f4e9
Oleksii Fedorov <waterlink000@gmail.com>
2015-07-17 05:30:10 +0300

    Test simple message producing with one consumer [red]

--- client/fanout.go
+++ client/fanout.go
@@ -1,1 +1,25 @@
 package fanout
+
+import (
+	_ "github.com/golang/protobuf/proto"
+)
+
+type Message struct {
+	Value []byte
+}
+
+type Consumer struct {
+	fanouts   []string
+	serviceId string
+	topic     string
+}
+
+func NewConsumer(fanouts []string, serviceId string, topic string) (Consumer, error) {
+	return Consumer{
+		fanouts:   fanouts,
+		serviceId: serviceId,
+		topic:     topic,
+	}, nil
+}
+
+func (c Consumer) Subscribe(fn func(raw Message)) {}
--- integration/simple_test.go
+++ integration/simple_test.go
@@ -3,7 +3,132 @@
 package integration
 
 import (
+	kafka "github.com/Shopify/sarama"
+	"github.com/golang/protobuf/proto"
+	fanout "github.com/nanoservice/core-fanout/client"
+	"github.com/nanoservice/core-fanout/integration/userneed"
+	"os"
+	"reflect"
 	"testing"
+	"time"
 )
 
-func TestNothing(t *testing.T) {}
+var (
+	messageA = &userneed.UserNeed{
+		NeedId: 7,
+		UserId: 17,
+		Sender: "test_producer",
+	}
+
+	messageB = &userneed.UserNeed{
+		NeedId: 9,
+		UserId: 3,
+		Sender: "test_producer",
+	}
+
+	messageC = &userneed.UserNeed{
+		NeedId: 84,
+		UserId: 94743,
+		Sender: "another_test_producer",
+	}
+
+	kafkas = []string{
+		os.Getenv("KAFKA_1_PORT_9092_TCP_ADDR") + ":" + os.Getenv("KAFKA_1_PORT_9092_TCP_PORT"),
+		os.Getenv("KAFKA_2_PORT_9092_TCP_ADDR") + ":" + os.Getenv("KAFKA_2_PORT_9092_TCP_PORT"),
+		os.Getenv("KAFKA_3_PORT_9092_TCP_ADDR") + ":" + os.Getenv("KAFKA_3_PORT_9092_TCP_PORT"),
+	}
+
+	fanouts = []string{
+		os.Getenv("FANOUT_1_PORT_4987_TCP_ADDR") + ":" + os.Getenv("FANOUT_1_PORT_4987_TCP_PORT"),
+		os.Getenv("FANOUT_2_PORT_4987_TCP_ADDR") + ":" + os.Getenv("FANOUT_2_PORT_4987_TCP_PORT"),
+		os.Getenv("FANOUT_3_PORT_4987_TCP_ADDR") + ":" + os.Getenv("FANOUT_3_PORT_4987_TCP_PORT"),
+	}
+)
+
+func TestOneConsumer(t *testing.T) {
+	inbox := subscriptionInbox(t)
+	producer := newProducer()
+
+	producer.Publish(messageA)
+	producer.Publish(messageB)
+	producer.Publish(messageC)
+
+	assertReceived(t, inbox, messageA)
+	assertReceived(t, inbox, messageB)
+	assertReceived(t, inbox, messageC)
+}
+
+func subscriptionInbox(t *testing.T) (inbox chan *userneed.UserNeed) {
+	inbox = make(chan *userneed.UserNeed)
+
+	newConsumer().Subscribe(func(raw fanout.Message) {
+		message := &userneed.UserNeed{}
+		err := proto.Unmarshal(raw.Value, message)
+		if err != nil {
+			t.Errorf("Unmarshal error: %v", err)
+		}
+		inbox <- message
+	})
+
+	return
+}
+
+func assertReceived(t *testing.T, inbox chan *userneed.UserNeed, expected *userneed.UserNeed) {
+	select {
+	case actual := <-inbox:
+		if !reflect.DeepEqual(*actual, *expected) {
+			t.Errorf("Expected %v to equal to %v", *actual, *expected)
+		}
+
+	case <-time.After(5 * time.Millisecond):
+		t.Errorf("Expected to receive %v, got nothing", *expected)
+	}
+}
+
+type AsyncProducer struct {
+	delegatee kafka.AsyncProducer
+}
+
+func (producer AsyncProducer) Publish(message proto.Message) {
+	topic := "test_topic"
+	raw, _ := proto.Marshal(message)
+	producer.delegatee.Input() <- &kafka.ProducerMessage{
+		Topic: topic,
+		Value: kafka.ByteEncoder(raw),
+	}
+}
+
+func newProducer() AsyncProducer {
+	var producer kafka.AsyncProducer
+	config := kafka.NewConfig()
+
+	_ = retry(func() (err error) {
+		producer, err = kafka.NewAsyncProducer(kafkas, config)
+		return
+	})
+
+	return AsyncProducer{producer}
+}
+
+func newConsumer() (consumer fanout.Consumer) {
+	_ = retry(func() (err error) {
+		consumer, err = fanout.NewConsumer(fanouts, "test_service", "test_topic")
+		return
+	})
+	return
+}
+
+func retry(fn func() error) error {
+	return retryCustom(10, 1*time.Second, fn)
+}
+
+func retryCustom(times int, interval time.Duration, fn func() error) (err error) {
+	for i := 0; i < times; i++ {
+		err = fn()
+		if err == nil {
+			return
+		}
+		time.Sleep(interval)
+	}
+	return
+}
