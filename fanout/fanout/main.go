1395308fd3bbf5e914b7fb94f58db59bde35bd65
Oleksii Fedorov <waterlink000@gmail.com>
2015-07-23 02:00:29 +0200

    Some nitpicking of consumer config

--- fanout/fanout/main.go
+++ fanout/fanout/main.go
@@ -96,6 +96,9 @@ func handleConsumer(consumer kafka.PartitionConsumer) {
 func newConsumer() (masterConsumer kafka.Consumer, consumers []kafka.PartitionConsumer) {
 	topic := "test_topic"
 	config := kafka.NewConfig()
+	config.Net.KeepAlive = 30 * time.Second
+	config.Consumer.Retry.Backoff = 25 * time.Millisecond
+
 	consumers = make([]kafka.PartitionConsumer, 0)
 
 	retry(func() (err error) {
